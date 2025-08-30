332b6d5d49423405b91b28d24f40dd21e6922d01
akhilankathi <91078213+akhilankathi@users.noreply.github.com>
2025-08-30 09:38:01 +0000

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   app.enableCors({
+    origin: [
+      "https://zany-robot-gwpw654r5g9f9vxx-5173.app.github.dev",   
+      "https://akhilankathi.netlify.app"
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
